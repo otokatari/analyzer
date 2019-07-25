12b8fbf2fa57f9f3db7ad5869ae689e5848241d8
Nemesiss.Lin <353388639@qq.com>
2019-07-25 21:04:19 +0800

    完成酷狗音乐自动下载分析逻辑，重新组织文件

---
+++ Analyzer/Music/MusicAnalyzer.cs
@@ -0,0 +1,58 @@
+using System.IO;
+using System.Text.RegularExpressions;
+using UserAnalyzer.Configurations;
+using UserAnalyzer.Model;
+
+namespace UserAnalyzer.Analyzer.Music
+{
+    public class MusicAnalyzer
+    {
+        private readonly AnalyzerConfig _config;
+        private readonly Regex BPMMatcher = new Regex("[0-9]+.[0-9]+");
+        public MusicAnalyzer(AnalyzerConfig config)
+        {
+            _config = config;
+        }
+
+        public void AnalyzeBPM(SongInfo info)
+        {
+            var SongFilePath = Path.Combine(_config.MusicDownloadPath, info.AudioFileName);
+            FileInfo file = new FileInfo(SongFilePath);
+            if (file.Exists)
+            {
+                int ExitCode = Utils.ExecuteCommand(_config.Aubio, out string stdout, out string stderr, "tempo", "-H 640", "-v", SongFilePath);
+                if (ExitCode == 0)
+                {
+                    var match = BPMMatcher.Match(stdout);
+                    info.BPM = match.Success ? (double?)double.Parse(match.Value) : null;
+                }
+                else
+                {
+                    System.Console.WriteLine($"aubio不正常退出 -- {ExitCode}");
+                }
+            }
+        }
+        public void AnalyzeLanguage(SongInfo info)
+        {
+            if(!string.IsNullOrEmpty(info.LyricFileName))
+            {
+                var LyricFilePath = Path.Combine("./lyrics",info.LyricFileName);
+                int ExitCode = Utils.ExecuteCommand("python",out string stdout,out string stderr,_config.LangDetector,LyricFilePath);
+                if(ExitCode == 0)
+                {
+                    info.Language = stdout;
+                    System.Console.WriteLine(stdout);
+                }
+                else
+                {
+                    System.Console.WriteLine($"{ExitCode} -- {stderr}");
+                }
+            }
+        }
+
+        public void PersistInfoToDb(SongInfo info)
+        {
+            
+        }
+    }
+}
---
+++ Analyzer/Music/MusicInfoCollector.cs
@@ -0,0 +1,42 @@
+
+using UserAnalyzer.Analyzer.Request;
+using UserAnalyzer.Configurations;
+using UserAnalyzer.Model;
+
+namespace UserAnalyzer.Analyzer.Music
+{
+    public class MusicInfoCollector
+    {
+        private readonly NeteaseRequest netease;
+        private readonly KugouRequest kugou;
+        private readonly QQMusicRequest qqmusic;
+        private readonly AnalyzerConfig _config;
+        public MusicInfoCollector(AnalyzerConfig config)
+        {
+            _config = config;
+            netease = new NeteaseRequest(_config);
+            qqmusic = new QQMusicRequest(_config);
+            kugou = new KugouRequest(_config);
+        }
+
+        public void GetSongInfo(SongInfo info)
+        {
+            switch(info.Platform)
+            {
+                case "netease":
+                    netease.GetSongInfo(info);
+                    break;
+                    
+                case "kugou":
+                    kugou.GetSongInfo(info);
+                    break;
+
+                case "qqmusic":
+                    qqmusic.GetSongAudio(info);
+                    break;
+            }
+        }
+    }
+
+
+}
